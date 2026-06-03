46bcf6e95843af002d839fbea57f545d6d050831
shivamchavan2322-del <shivamchavan2322@gmail.com>
2026-06-03 23:34:56 +0530

    Add files via upload

---
+++ intern1.js
@@ -0,0 +1,11 @@
+// 1. VARIABLE
+const name = "shivam";
+// 2. FUNCTION
+function greet(person) {
+return "Hello, " + person + "! Welcome to Nexora.";
+}
+// 3. LOOP
+const students = ["shivam", "abdullu", "Chris", "yashas"];
+for (let i = 0; i < students.length; i++) {
+console.log( greet(students[i]) );
+} 
